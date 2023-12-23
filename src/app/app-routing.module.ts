c43cffae22c034e7f67236159ddc55f84f1a14f7
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-12-23 10:03:08 +0100

    samedi 23 dec 2023 10 h avant mentorat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,29 @@
-import { NgModule } from '@angular/core';                    // NgModules sont des conteneurs pour un groupe cohérent de classes avec des fonctionnalités connexes. Ils sont utilisés pour organiser et structurer les applications Angular.
-import { RouterModule, Routes } from '@angular/router';      // Le RouterModule est utilisé pour configurer les routes de l'application, et l'interface Routes est utilisée pour définir les routes elles-mêmes.
-import { HomeComponent } from './pages/home/home.component'; // C'est le composant qui sera chargé lorsque l'utilisateur navigue vers le chemin d'accès de la page d'accueil.
-import { NotFoundComponent } from './pages/not-found/not-found.component'; // Ce composant sera utilisé pour afficher une page "404 Not Found" lorsque l'URL ne correspond à aucune route définie.
-import { DetailsComponent } from './pages/details/details.component';
-const routes: Routes = [                                     // Déclare une constante routes de type Routes (un tableau de routes).
-  {
-    path: '',                                                // Définit une route. Quand le chemin d'accès (path) est vide ('')
-    component: HomeComponent,                                // le HomeComponent est chargé. Cela correspond généralement à la page d'accueil.
-  },
-  {
-    path: 'details/:id',                                     // Définit la route vers la page details selon l'id demandé (pays)
-    component: DetailsComponent,                             // le DetailsComponent est chargé. C'est le composant de la page Details.
-  },
-  {
-    path: '**',                                              // wildcard est utilisé (**) pour définir une route. Cette route correspond à tout chemin non défini ailleurs dans la configuration. 
-    component: NotFoundComponent,                            // Elle charge NotFoundComponent, utile pour afficher une page d'erreur lorsque l'URL demandée n'existe pas. 404
-  },
+import { NgModule } from "@angular/core"; // NgModules sont des conteneurs pour un groupe cohérent de classes avec des fonctionnalités connexes. Ils sont utilisés pour organiser et structurer les applications Angular.
+import { RouterModule, Routes } from "@angular/router"; // Le RouterModule est utilisé pour configurer les routes de l'application, et l'interface Routes est utilisée pour définir les routes elles-mêmes.
+import { HomeComponent } from "./pages/home/home.component"; // C'est le composant qui sera chargé lorsque l'utilisateur navigue vers le chemin d'accès de la page d'accueil.
+import { NotFoundComponent } from "./pages/not-found/not-found.component"; // Ce composant sera utilisé pour afficher une page "404 Not Found" lorsque l'URL ne correspond à aucune route définie.
+import { DetailsComponent } from "./pages/details/details.component";
+const routes: Routes = [
+	// Déclare une constante routes de type Routes (un tableau de routes).
+	{
+		path: "", // Définit une route. Quand le chemin d'accès (path) est vide ('')
+		component: HomeComponent, // le HomeComponent est chargé. Cela correspond généralement à la page d'accueil.
+	},
+	{
+		path: "details/:id", // Définit la route vers la page details selon l'id demandé (pays)
+		component: DetailsComponent, // le DetailsComponent est chargé. C'est le composant de la page Details.
+	},
+	{
+		path: "**", // wildcard est utilisé (**) pour définir une route. Cette route correspond à tout chemin non défini ailleurs dans la configuration.
+		component: NotFoundComponent, // Elle charge NotFoundComponent, utile pour afficher une page d'erreur lorsque l'URL demandée n'existe pas. 404
+	},
 ];
 
-@NgModule({                                                  // Décorateur @NgModule qui marque la classe comme un module Angular.
-  imports: [RouterModule.forRoot(routes)],                   // Ajoute RouterModule au tableau des imports du module, avec la configuration de route (routes) initialisée via forRoot. forRoot configure le routeur au niveau racine de l'application.
-  exports: [RouterModule],                                   // Exporte RouterModule pour qu'il soit disponible dans d'autres parties de l'application.
+@NgModule({
+	// Décorateur @NgModule qui marque la classe comme un module Angular.
+	imports: [RouterModule.forRoot(routes)], // Ajoute RouterModule au tableau des imports du module, avec la configuration de route (routes) initialisée via forRoot. forRoot configure le routeur au niveau racine de l'application.
+	exports: [RouterModule], // Exporte RouterModule pour qu'il soit disponible dans d'autres parties de l'application.
 })
-export class AppRoutingModule {}                             // Déclare et exporte la classe AppRoutingModule, qui est le module de routage de l'application.
-
-
+export class AppRoutingModule {} // Déclare et exporte la classe AppRoutingModule, qui est le module de routage de l'application.
 
 // Chaque ligne contribue à la mise en place et à la configuration du système de routage dans une application Angular, permettant ainsi une navigation fluide entre différents composants ou vues.
